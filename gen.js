d2d04221be1248c5b04047e3f090a9e4eea08797
puqeko <puqeko@gmail.com>
2022-10-09 23:30:01 +1300

    Integrate to gen.js

--- gen.js
+++ gen.js
@@ -1,142 +1,47 @@
 import fs from 'fs-extra';
 import inq from 'inquirer';
-import {assignIdleWorker, initWorkers, workerStats, stopWorkers, __dirname} from './workerutils.js'
+import { Sequelize } from 'sequelize';
+import { generate as genstd } from './genstd.js';
+import { generate as genguide } from "./genguide.js";
 
-const log = console.log;
-const err = console.error;
-
-const BASE_URL = "https://ziglang.org/documentation/";
-const DOC_NAME = "zig.docset"
-const DOC_PATH = DOC_NAME + "/Contents/Resources/Documents/";
-
-const PACK_SIZE = 25;
-let packs = [];  // groups of PACK_SIZE for processing
-const divide = () => {
-  const last = packs.pop();
-  const div = [[], []];
-  for (let i = 0; i < last.length; i++) div[i%2].push(last[i]);
-  packs.push(...div);
-};
-
-const seen = new Set();
-const libs = [];
-const displayCounts = () => {
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  let pn = 0;
-  for (const p of packs) pn += p.length;
-  let idleCount = 0;
-  for (const ws of workerStats()) if (ws.status == "idle") idleCount += 1;
-  process.stdout.write(`Seen: ${seen.size} Have: ${pn + libs.length} Idle: ${idleCount}`);
-};
-
-const allocate = () => {
-  if (libs.length > 0) {
-    return {lib: libs.pop()};
-  } if (packs.length > 0) {
-    const pack = packs.at(0);  // take from front
-    packs = packs.slice(1);
-    return {pack};
-  }
-};
-const doStd = (baseUrl) => {
-  initWorkers(__dirname + "/stdworker.js", {
-    nWorkers: 3,
-    initData: {baseUrl, DOC_NAME},
-    onInit: (o) => {
-      const {msgData} = o;
-      for (const lib of msgData) {
-        if (seen.has(lib.hash)) continue;
-        seen.add(lib.hash);
-        libs.push(lib);
-      }
-    },
-    onIdle: (o) => {
-      const next = allocate();
-      if (next) o.assign(next);
-
-      let idleCount = 0;
-      for (const ws of workerStats()) if (ws.status == "idle") idleCount += 1;
-      if (idleCount == workerStats().length) {
-        stopWorkers();
-        log("\nDone");
-      }
-      while (idleCount > 0) {
-        const next = allocate();
-        if (next) assignIdleWorker(next);
-        else break;
-        idleCount--;
-      }
-      displayCounts();
-    },
-    onData: (o) => {
-      const {msgData} = o;
-      if (packs.length == 0) packs.push([]);
-      let nfull = packs.map((p) => p.length >= PACK_SIZE).reduce((a, b) => a+b);
-      if (nfull == packs.length) {
-        divide(packs);
-        if (nfull > 0) nfull -= 1;
-      }
-      let i = 0;
-      for (const f of msgData) {
-        if (seen.has(f.hash)) continue;
-        seen.add(f.hash);
-        packs[nfull + (i % (packs.length - nfull))].push(f);
-        if (packs.at(-1).length >= PACK_SIZE) {
-          nfull += 1;
-          if (nfull == packs.length) {
-            divide(packs);
-            if (nfull > 0) nfull -= 1;
-          }
-        }
-        i += 1;
-      }
-
-      let idleCount = 0;
-      for (const ws of workerStats()) if (ws.status == "idle") idleCount += 1;
-      if (idleCount == workerStats().length) {
-        stopWorkers();
-        log("\nDone");
-      }
-      while (idleCount > 0) {
-        const next = allocate();
-        if (next) assignIdleWorker(next);
-        else break;
-        idleCount--;
-      }
-      displayCounts();
-    }
-  });
-}
-
-doStd("https://ziglang.org/documentation/0.9.1/std/");
-
-// TODO: try sending batches back instead
-// improve ready checker for multi worker
-
-async function doLangRef(url) {
-  return;
-}
+const BASE_URL = new URL("https://ziglang.org/documentation/");
 
 const main = async () => {
+
+  let docName = (await inq.prompt({
+    type: 'text',
+    name: 'res',
+    message: "Docset name",
+    default: "zig.docset"
+  })).res;
+
+  if (!docName.endsWith(".docset")) docName += ".docset";
+
   let shouldInit = true;
-  if (fs.existsSync(DOC_NAME)) {
+  if (fs.existsSync(docName)) {
     const res = (await inq.prompt({
-      type: "confirm",
+      type: "list",
       name: "res",
-      message: `Will merge with '${DOC_NAME}' or replace it?`,
+      message: `Merge with existing '${docName}' or replace it?`,
       choices: ["Merge", "Replace", "Canel"]
     })).res;
     if (res == "Cancel") return;
-    if (res == "Replace") await fs.rm(DOC_NAME, {recursive: true});
+    if (res == "Replace") await fs.rm(docName, {recursive: true});
     shouldInit = res == "Replace";
   }
+
   if (shouldInit) {
+    const db = new Sequelize({
+      dialect: 'sqlite',
+      logging: false,
+      storage: `${docName}/Contents/Resources/docSet.dsidx`
+    });
+    await fs.copy("template", docName);
     await db.query(`CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);`);
     await db.query(`CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);`);
-    await fs.copy("template", DOC_NAME);
   }
-  const langRefUrl = BASE_URL.href + "/" + (await inq.prompt({
+
+  const langRefUrl = BASE_URL.href + (await inq.prompt({
     type: "list",
     name: "ver",
     message: `Version?`,
@@ -151,8 +56,8 @@ const main = async () => {
       {name: "Zig Standard Library", checked: true}]
   })).res;
 
-  if (todo.includes("Zig Language Reference")) await doLangRef(langRefUrl);
-  if (todo.includes("Zig Standard Library")) await doStd(langRefUrl + "std/");
+  if (todo.includes("Zig Language Reference")) await genguide(langRefUrl, docName);
+  if (todo.includes("Zig Standard Library")) await genstd(langRefUrl + "std/", docName);
 };
 
-// main();
+main();
